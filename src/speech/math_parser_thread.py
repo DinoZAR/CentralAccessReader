a9136118d6d75c0fbb863e36f48391ea09dcb37d
Spencer Graffe <sgraffe@gmail.com>
2013-07-01 09:50:46 -0700

    Made a math parsing thread that will constantly parse MathML and store it so that it is all ready immediately.

--- src/speech/math_parser_thread.py
+++ src/speech/math_parser_thread.py
@@ -5,33 +5,47 @@ Created on Jun 28, 2013
 '''
 from PyQt4.QtCore import QThread, QMutex, pyqtSignal
 
-
 class MathParserThread(QThread):
     '''
     This thread is used to continuously parse math equations and then post them
     to the assigner when they are done.
     '''
+    mathParsed = pyqtSignal(list)
     
-    mathParsed = pyqtSignal(dict)
-    
-    queueLock = QMutex()
-    mathDatabaseLock = QMutex()
-
-    def __init__(self, mathDatabase):
+    def __init__(self, mathTTS):
         QThread.__init__(self)
-        self._mathDatabase = mathDatabase
+        self._mathTTS = mathTTS
         self._mathQueue = []
+        
+        self.queueLock = QMutex()
+        self.mathTTSLock = QMutex()
     
     def run(self):
-        pass
+        while True:
+            if self._mathTTS != None:
+                # Keep using the math database to parse math equations. Then, 
+                # publish the parsed math out to whatever needs it.
+                self.queueLock.lock()
+                math = self._mathQueue.pop(0)
+                self.queueLock.unlock()
+                
+                self.mathTTSLock.lock()
+                prose = self._mathTTS.parse(math[1]['mathml'])
+                self.mathTTSLock.unlock()
+                
+                math[1]['parsed'] = True
+                math[1]['prose'] = prose
+                
+                self.mathParsed.emit(math)
     
-    def setMathDatabase(self, newMathDatabase):
+    def setMathDatabase(self, newMathTTS):
         '''
-        Changes the math database that this parser is using.
+        Changes the math database that this parser is using. It should be a 
+        MathTTS object.
         '''
-        self.mathDatabaseLock.lock()
-        self._mathDatabase = newMathDatabase
-        self.mathDatabaseLock.unlock()
+        self.mathTTSLock.lock()
+        self._mathTTS = newMathTTS
+        self.mathTTSLock.unlock()
         
     def addToQueue(self, mathData):
         '''
@@ -40,3 +54,13 @@ class MathParserThread(QThread):
         self.queueLock.lock()
         self._mathQueue.append(mathData)
         self.queueLock.unlock()
+        
+    def clearQueue(self):
+        '''
+        Removes all of the items in its queue.
+        '''
+        self.queueLock.lock()
+        self._mathQueue = []
+        self.queueLock.unlock()
+        
+        
