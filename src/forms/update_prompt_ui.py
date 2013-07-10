80df3ed4d97b944ae310b71b356ab67becd2b920
Spencer Graffe <sgraffe@gmail.com>
2013-07-10 08:42:58 -0700

    Changed mechanics of updater. Now it will prompt user to download update it they haven't already. There is a progress bar on the bottom to show the progress.

--- src/forms/update_prompt_ui.py
+++ src/forms/update_prompt_ui.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'W:\Nifty Prose Articulator\workspace2\another\src\forms/update_prompt.ui'
+# Form implementation generated from reading ui file 'W:\Nifty Prose Articulator\workspace\nifty-prose-articulator\src\forms/update_prompt.ui'
 #
-# Created: Mon Jul 08 13:14:51 2013
+# Created: Tue Jul 09 16:40:20 2013
 #      by: PyQt4 UI code generator 4.10
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,7 +26,7 @@ except AttributeError:
 class Ui_UpdatePromptDialog(object):
     def setupUi(self, UpdatePromptDialog):
         UpdatePromptDialog.setObjectName(_fromUtf8("UpdatePromptDialog"))
-        UpdatePromptDialog.resize(355, 177)
+        UpdatePromptDialog.resize(439, 164)
         font = QtGui.QFont()
         font.setPointSize(12)
         UpdatePromptDialog.setFont(font)
@@ -52,10 +52,10 @@ class Ui_UpdatePromptDialog(object):
         self.horizontalLayout.addWidget(self.label)
         self.horizontalLayout.setStretch(1, 1)
         self.verticalLayout_3.addLayout(self.horizontalLayout)
-        self.label_3 = QtGui.QLabel(UpdatePromptDialog)
-        self.label_3.setWordWrap(True)
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.verticalLayout_3.addWidget(self.label_3)
+        self.contentText = QtGui.QLabel(UpdatePromptDialog)
+        self.contentText.setWordWrap(True)
+        self.contentText.setObjectName(_fromUtf8("contentText"))
+        self.verticalLayout_3.addWidget(self.contentText)
         self.horizontalLayout_2 = QtGui.QHBoxLayout()
         self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
@@ -83,9 +83,9 @@ class Ui_UpdatePromptDialog(object):
         UpdatePromptDialog.setTabOrder(self.noButton, self.yesButton)
 
     def retranslateUi(self, UpdatePromptDialog):
-        UpdatePromptDialog.setWindowTitle(_translate("UpdatePromptDialog", "Dialog", None))
+        UpdatePromptDialog.setWindowTitle(_translate("UpdatePromptDialog", "Install Update", None))
         self.label.setText(_translate("UpdatePromptDialog", "Update is Available!", None))
-        self.label_3.setText(_translate("UpdatePromptDialog", "Would you like to update the program? The Central Access Reader must close in order to run installer.", None))
+        self.contentText.setText(_translate("UpdatePromptDialog", "Would you like to download it?", None))
         self.yesButton.setText(_translate("UpdatePromptDialog", "Yes", None))
         self.noButton.setText(_translate("UpdatePromptDialog", "No", None))
 
