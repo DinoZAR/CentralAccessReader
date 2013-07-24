41a71b5f1e6ab9849fd6a335ef63d647d725d4de
Spencer Graffe <sgraffe@gmail.com>
2013-07-24 15:51:03 -0700

    Added cancel hook on image saving thread.

--- src/docx/importer.py
+++ src/docx/importer.py
@@ -22,12 +22,18 @@ ROOT_PATH = program_path('src/docx')
 w_NS = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
 r_NS = '{http://schemas.openxmlformats.org/package/2006/relationships}'
 
-def save_images(docxPath, importPath):
+def save_images(docxPath, importPath, cancelHook=None):
      
     # Open a zip file of my docx file
     z = zipfile.ZipFile(docxPath, 'r')
  
     for f in z.namelist():
+        
+        # Check for cancel
+        if cancelHook is not None:
+            if cancelHook():
+                break
+        
         if f.find('word/media/') == 0:
             # Extract it to my import folder
             savePath = importPath + '/images/' + f.replace('word/media/', '')
@@ -81,7 +87,7 @@ class DocxDocument(object):
         print 'Starting the save images thread...'
         
         # Start saving images to my import folder in the background
-        saveImagesThread = Thread(target=save_images, args=(docxFilePath, self.importFolder))
+        saveImagesThread = Thread(target=save_images, args=(docxFilePath, self.importFolder, cancelHook))
         saveImagesThread.start()
         
         # .docx is just a zip file
