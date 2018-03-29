1ff081b4edcb0ad48fca38543908ccca9a562ee2
Spencer Graffe <spencer@Spencers-MBP.attlocal.net>
2018-03-29 07:17:28 -0700

    Ended up using my own config

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname + '/src/renderer.js'),
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname + '/dist'),
+    filename: '[name]-[hash].js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      // {
+      //   test: /\.(css|min\.css)$/,
+      //   use: ['style-loader', 'css-loader']
+      // },
+      // {
+      //   test: /\.(png|jpg|gif|woff|woff2|ttf|eot|svg)$/,
+      //   use: ['file-loader']
+      // }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname + '/src/index.html'),
+      filename: 'index.html',
+      inject: true,
+      appMountID: 'root'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    // port: 8000,
+    // inline: true,
+    // host: '0.0.0.0',
+    // proxy: {
+    //   '/cms': {
+    //     target: 'http://directus:8080',
+    //     pathRewrite: { '^/cms': '/' },
+    //   },
+    //   '/storage': {
+    //     target: 'http://directus:8080',
+    //   }
+    // }
+  }
+}
